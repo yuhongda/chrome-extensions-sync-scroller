7a4484186e94b914e279553b7a3b1b6f1535c652
yuhongda <silverage.y@gmail.com>
2023-11-22 13:17:20 +0800

    feat: update pos on storage synced

--- background.js
+++ background.js
@@ -34,10 +34,12 @@ const updatePos = (request, sender, sendResponse) => {
     const _index = _pos.findIndex(item => item.url.split('#')[0] === sender.tab.url.split('#')[0])
     if (_index !== -1) {
       _pos[_index].y = request.posY * (request.scale || 1)
+      _pos[_index].scale = request.scale || 1
     } else {
       _pos.push({
         url: sender.tab.url,
         y: request.posY * (request.scale || 1),
+        scale: request.scale || 1,
       })
     }
 
@@ -57,4 +59,17 @@ chrome.tabs.onUpdated.addListener(function (request, sender, sendResponse) {
 chrome.tabs.onHighlighted.addListener(function (request, sender, sendResponse) {
   updatePos(request, sender, sendResponse)
   return true
+})
+
+chrome.storage.onChanged.addListener((changes, areaName) => {
+  console.log(changes, areaName)
+  const { pos } = changes
+  if (!pos) return
+  const { newValue, oldValue } = pos
+  if (!newValue || !oldValue) return
+  // find the item that has different value of the key 'y' in newValue and oldValue
+  const diff = newValue.find(item => oldValue.find(oldItem => oldItem.url === item.url && oldItem.y !== item.y))
+  if (!diff) return
+  const { url, y, scale } = diff
+  updatePos({ posY: y, scale }, { tab: { url } }, null)
 })
